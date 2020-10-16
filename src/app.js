f55bba72dd768c08e9d4c05da18521da674ac9d6
Saadh S <atextbooksituation@gmail.com>
2020-10-16 10:16:57 -0400

    What was done a few weeks ago. Nothing concrete yet

--- src/app.js
+++ src/app.js
@@ -1,20 +1,33 @@
-const puppeteer = require( 'puppeteer' );
+const puppeteer = require( 'puppeteer-core' );
 const axios = require( 'axios' );
 
-const CHROME_PORT = 9222
-const response = await axios.get( `http://localhost:${ CHROME_PORT }/json/version` );
-const { webSocketDebuggerUrl } = response.data;
-const browser = await puppeteer.connect( { browserWSEndpoint: webSocketDebuggerUrl } );
-console.info( browser );
+const browserFunc = async () => {
+  const CHROME_PORT = 9222
+  const response = await axios.get( `http://localhost:${ CHROME_PORT }/json/version` );
+  const { webSocketDebuggerUrl } = response.data;
+  return await puppeteer.connect( { browserWSEndpoint: webSocketDebuggerUrl } );  
+};
 
-// Only one tab. Not sure if this is needed
-const pages = await browser.pages();
-const curr = pages[ 0 ];
+( async () => {
+  const browser = await browserFunc();
+  // Only one tab. Not sure if this is needed
+  const pages = await browser.pages();
+  const page = pages[ 0 ];
+  
+  console.log( 'lol1' );
+  // await page.waitForSelector( 'button.join-dialog__close' );
+  await page.click( 'button.join-dialog__close' );
+  
+  // Waits until the `title` meta element is rendered
+  await page.waitForSelector('title');
+  // Fetches page's title
+  const title = await page.title();
+  console.info(`The title 1: ${title}`);
+  console.info(`The title 3: ${title}`);
 
-console.log( 'lol1' );
-debugger;
-console.log( 'lol2' );
+  
+  // Probably never run the below?
+  // await browser.close();
+  // await chrome.kill();
+} )();
 
-// Probably never run the below?
-// await browser.close();
-// await chrome.kill();
