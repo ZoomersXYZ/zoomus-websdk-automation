9db591166040f16cdd3e27592d12400121121eb0
Saadh S <atextbooksituation@gmail.com>
2020-11-20 23:21:12 -0500

    Moved the original classes/structure code over to own file. Deal with later

--- src/app.js
+++ src/app.js
@@ -16,55 +16,6 @@ async function browserFunc() {
   return await puppeteer.connect( { browserWSEndpoint: webSocketDebuggerUrl } );  
 };
 
-class Dom {
-  constructor( thePage ) {
-    this.S = ' ';
-    this.page = thePage;
-  };
-};
-
-class MeetingControls extends Dom {
-  rootDom = 'footer#wc-footer';
-  participantsDom = this.rootDom + ' ' + 'div.footer-button__participants-icon';
-  
-  async activateParticipants() {
-  };
-
-  async deactivateParticipants() {
-  };
-};
-
-class Participants extends Dom {
-  static rootDom = 'div#wc-container-right';
-  static siblingheaderDom = this.rootDom + ' ' + 'div.participants-header__header';
-};
-
-class ParticipantsMore extends Participants {
-};
-
-class ChatBox extends Dom {
-};
-
-class Initial extends Dom {
-  rootDom = 'div.join-dialog';
-  middleDom = ' div[role="tablist"]';
-  closeButtonDom = ' > button[type="button"]';
-  closeDialogDom = this.rootDom + this.middleDom + this.closeButtonDom;
-
-  async closeDialog() {
-  };
-};
-
-class Doc {
-  constructor( page ) {
-    this.controls = new MeetingControls( page );
-    this.people = new Participants( page );
-    this.more = new ParticipantsMore( page );
-    this.chat = new ChatBox( page );
-    this.initial = new Initial( page );
-  };
-};
-
 async function run() {
   // Functions
   async function isVisibleCommand( sel ) {
