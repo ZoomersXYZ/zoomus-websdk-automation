c0963ec5ca1eb3e599acb78a4cadb139e1d9fd00
Saadh S <atextbooksituation@gmail.com>
2020-11-23 18:16:33 -0500

    A few oops/minor issues
    - Accidentally had packages installed outside this git committed area
    - Wasn’t awaiting the core action methods requiring synchronous actions

--- src/app.js
+++ src/app.js
@@ -18,6 +18,8 @@ async function run() {
   const a = new Automation( page );
   let sel = undefined;
 
+  logger.info( '----- THE BEGINNING -----' );
+
   // Reload
   await page.reload( { 
     waitUntil: [ 'networkidle0', 'domcontentloaded' ] } 
@@ -28,24 +30,24 @@ async function run() {
   ////
 
   sel = '#root > div > .form > .MuiButtonBase-root > .MuiButton-label';
-  a.selClick( sel );
+  await a.selClick( sel );
 
   // Zoom Room
 
   sel = '.meeting-client-inner > .join-dialog > div > .zmu-tabs > .zmu-tabs__tab-container';
-  a.selClick( sel );
+  await a.selClick( sel );
   
   sel = '.zmu-tabs > .zmu-tabs__tab-container > .zmu-tabs__tabs-list > #voip > .tab-bar-node';
-  a.selClick( sel );
+  await a.selClick( sel );
   
   sel = '.zmu-tabs > .zmu-tabs__tab-container > .zmu-tabs__tabs-list > #phone > .tab-bar-node';
-  a.selClick( sel );
+  await a.selClick( sel );
   
   sel = '.zmu-tabs__tab-container > .zmu-tabs__tabs-list > #voip > .tab-bar-node > .tab-bar-node__text';
-  a.selClick( sel );
+  await a.selClick( sel );
   
   sel = '.join-dialog > div > .zmu-tabs > .zmu-tabs__tab-container > .zm-btn';
-  a.selClick( sel );
+  await a.selClick( sel );
 };
 
 run();
