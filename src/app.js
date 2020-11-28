1397587e65cfa56f4c3135255bb22120cc7784f9
Saadh S <atextbooksituation@gmail.com>
2020-11-28 08:55:17 -0500

    Moved the automation Puppeteer actions to own file

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const Automation = require( './Automation' );
 
 const logger = require( './config/logger' );
 
+const initialLaunch = require( './initialLaunch' );
+
 async function run() {
   // Initializaing
   const browser = await browserFunc();
@@ -10,7 +12,7 @@ async function run() {
   const page = pages[ 0 ];  
 
   // Initializaing values
-  await page.setDefaultTimeout( 5000 );
+  await page.setDefaultTimeout( 7500 );
   await page.setDefaultNavigationTimeout( 30000 );
   await page.setViewport( { width: 1200, height: 900 } );  
 
@@ -24,30 +26,8 @@ async function run() {
   await page.reload( { 
     waitUntil: [ 'networkidle0', 'domcontentloaded' ] } 
   );
-  
-  ////
-  // Actual automation
-  ////
 
-  sel = '#root > div > .form > .MuiButtonBase-root > .MuiButton-label';
-  await a.selClick( sel );
-
-  // Zoom Room
-
-  sel = '.meeting-client-inner > .join-dialog > div > .zmu-tabs > .zmu-tabs__tab-container';
-  await a.selClick( sel );
-  
-  sel = '.zmu-tabs > .zmu-tabs__tab-container > .zmu-tabs__tabs-list > #voip > .tab-bar-node';
-  await a.selClick( sel );
-  
-  sel = '.zmu-tabs > .zmu-tabs__tab-container > .zmu-tabs__tabs-list > #phone > .tab-bar-node';
-  await a.selClick( sel );
-  
-  sel = '.zmu-tabs__tab-container > .zmu-tabs__tabs-list > #voip > .tab-bar-node > .tab-bar-node__text';
-  await a.selClick( sel );
-  
-  sel = '.join-dialog > div > .zmu-tabs > .zmu-tabs__tab-container > .zm-btn';
-  await a.selClick( sel );
+  await initialLaunch( a );
 };
 
 run();
---
+++ src/initialLaunch.js
@@ -0,0 +1,26 @@
+async function initialLaunch( a ) {
+  let sel = undefined;
+  let rootSel = '';
+  let parentSel = '';
+  let combo = '';
+  logger.info( '-- initialLaunch BEGINNING --' );
+
+  // Launch meeting page
+  sel = '#root > div > .form > .MuiButtonBase-root > .MuiButton-label';
+  await a.selClick( sel );
+  
+  rootSel = '.join-dialog > div > .zmu-tabs > .zmu-tabs__tab-container > .zmu-tabs__tabs-list';
+  // Next 2 are just to click things just to make sure things are working
+
+  sel = ' > #voip > .tab-bar-node';
+  await a.selClick( rootSel + sel );
+  
+  sel = ' > #phone > .tab-bar-node';
+  await a.selClick( rootSel + sel );
+  
+  // close the overlay
+  sel = ' .zmu-tabs__tab-container > .zm-btn';
+  await a.selClick( rootSel + sel );
+};
+
+initialLaunch();
