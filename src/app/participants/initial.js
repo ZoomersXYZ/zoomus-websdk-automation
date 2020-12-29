9876183dddf1aa038d7e85605b838b58ad7b4c45
Saadh S <atextbooksituation@gmail.com>
2020-12-29 16:20:16 -0500

    Participant is doing most of what chat initial was doing with popping out

--- src/app/participants/initial.js
+++ src/app/participants/initial.js
@@ -13,29 +13,72 @@ async function participantsInitial( bootstrapBool = false ) {
   a.TIMEOUT = timeOut;
   a.PAUSE = pause;
 
+  ////
+  // STEP ONE: Check if popped out already
+  ////
+  parentSel = '#participant-window .participant-scrollbar ';
+  const partPopped = await a.visibleCheck( parentSel );
+  if ( partPopped ) {
+    logger.info( 'participants already popped out' );
+    return true;
+  };
+
   await a.page.waitForTimeout( pause );
+
+  ////
+  // STEP TWO: Check if on the right hand side already (unlikely)
+  // STEP THREE: If not, then click the footer button
+  ////
   // what: visible; chat
-  sel = '[aria-label="participant list"]';
-  const usersOpen = await a.visibleCheck( sel );
-  if ( !usersOpen ) {
+  parentSel = '#wc-container-right';
+  sel = ' #participantSectionMenu';
+  combo = parentSel + sel;
+  const partOpenToRight = await a.visibleCheck( combo );
+  if ( !partOpenToRight ) {
     // what: click; footer chat icon
-    sel = '#wc-footer > div > .footer-button__button > .footer-button__img-layer > .footer-button__participants-icon';
+    sel = '#wc-footer > div > .footer-button__button > .footer-button__img-layer > .footer-button__participants-icon'; 
     await a.selClick( sel );
   };
-  
   await a.page.waitForTimeout( pause );
-  // what: visible; chat on right side
-  parentSel = '#wc-container-right';
-  sel = ' [aria-label="participant list"]';
-  combo = parentSel + sel;
-  const usersRightSide = await a.visibleCheck( combo );
-  // If chat isn't popped out, pop it out
-  if ( !usersRightSide ) {
-    logger.warning( 
-      'participants panel not showing up on the right hand side' 
-    );
-    return false;
+
+  ////
+  // STEP FOUR: Check if popped out again - post footer button
+  ////
+  // Check if popped out now
+  parentSel = '#participant-window .participant-scrollbar ';
+  const partPopped = await a.visibleCheck( parentSel );
+  if ( partPoppedDeux ) {
+    logger.info( 'participants popped out deux' );
+    return true;
   };
+
+  ////
+  // STEP FIVE: Check if on right side again - post button.
+  // If not, shit is broken. Abort!
+  // STEP SIX: If it is, pop it out
+  ////
+  // Check if on right side again now
+  parentSel = '#wc-container-right';
+  sel = ' #participantSectionMenu';
+  combo = parentSel + sel;
+  const partOpenToRightDeux = await a.visibleCheck( combo );
+  if ( !partOpenToRightDeux ) {
+    logger.warn( 'participants isn\'t open' );
+  } else {
+    // what: visible; chat on right side
+    const usersRightSide = await a.visibleCheck( combo );
+    if ( usersRightSide ) {
+      // what: click; dropdown menu icon
+      await a.selClick( combo );
+      // Or manually go to it via keyboard
+      await a.page.waitForTimeout( 1000 );
+      await a.page.keyboard.press( 'ArrowDown' );
+      await a.page.waitForTimeout( 500 );
+      await a.page.keyboard.press( 'ArrowDown' );
+      await a.page.waitForTimeout( 500 );
+      await a.page.keyboard.press( 'Enter' );
+    };
+  }
   
   return true;
 };
