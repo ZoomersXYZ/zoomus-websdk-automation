ae5bf9113ce4569adbf05494b64dc0ac04acee80
Saadh S <atextbooksituation@gmail.com>
2020-12-29 16:21:26 -0500

    Grabbing visible (in DOM) chat text

--- src/app/chat/write.js
+++ src/app/chat/write.js
@@ -1,10 +1,12 @@
+const cheerio = require( 'cheerio' );
+
 const createLogger = require( '../config/createLogger' );
 
 const bootstrap = require( '../core/bootstrap' );
 const initial = require( './initial' );
 
 async function chatWrite() {
-  let sel, rootSel, parentSel, combo = '';
+  let sel, rootSel, parentSel, combo, $ = '';
   let pause = 1000;
   let timeOut = 5000;
   
@@ -27,7 +29,7 @@ async function chatWrite() {
     } );
   };
 
-  const toSelect = async function( ogSel, text ) { 
+  const toSelect = async function( ogSel, text ) {
     await a.page.$$eval( ogSel, ( els, text ) => {
       return els
         .find( el => 
@@ -42,6 +44,48 @@ async function chatWrite() {
   combo = sel + childSel;
   await toSelection( combo );
   await toSelect( sel, 'lol' );
+
+  // new stuff
+
+  // hover to grab timestamps as well
+  sel = 'div.ReactVirtualized__Grid__innerScrollContainer';
+  a.page.hover( sel );
+
+  // grab all the 
+  sel = 'div.chat-item__chat-info';
+  const theHtml = await page.evaluate( ( sel ) => 
+    Array.from( 
+      document.querySelectorAll( sel ), 
+      element => element.outerHTML 
+    ), sel 
+  );
+
+  const chatHistory = theHtml.map( ( solo ) => {
+    $ = cheerio.load( solo );
+    
+    const cSender = $( 'span:nth-child( 1 )' );
+    const sender = { 
+      id: msg.attr( 'data-userid' ), 
+      name: msg.attr( 'data-name' ) 
+    };
+    // const name = msg.attr( 'title' );
+
+    const cToWhom = $( 'span:nth-child( 2 )' );    
+    const toWhom = { 
+      id: cToWhom.attr( 'data-userid' ), 
+      name: cToWhom.attr( 'data-name' ) 
+    };
+
+    const time = $( '.chat-item__chat-info-time-stamp' ).text();
+    const msg = $( '.chat-item__chat-info-msg' ).text();
+
+    return {
+      sender, 
+      toWhom, 
+      time, 
+      msg 
+    };
+  } );
 };
 
 module.exports = chatWrite;
