d5db329f863968e983b514d3ef6e51a6baf1de5b
Saadh S <atextbooksituation@gmail.com>
2021-06-12 18:05:34 -0400

    Moving common code that can be shared for any Zoom automation out

--- src/app/util/index.js
+++
@@ -1,6 +0,0 @@
-exports.to = promise => promise.then( x => [ null, x ] ).catch( e => [ e ] );
-
-// const to = <T>(promise: Promise<T>): Promise<[null | Error, T | undefined]> =>
-//   promise
-//     .then((res) => [null, res] as [null, T])
-//     .catch((e) => [e, undefined] as [Error, undefined]);
